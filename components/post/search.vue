9270bb6739e4764623ea8a6813f370f30b97d810
matrix01 <wtlee4511@163.com>
2019-07-06 09:44:06 +0800

    攻略页样式

---
+++ components/post/search.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="searchbox">
+  
+        <input type="text" placeholder="请输入想去的地方，例如：广州" class="search"><i class="el-icon-search icon"></i>
+        <p>推荐：</p>
+
+        <div class="recommend">
+            <el-row type="flex" class="row-bg" justify="space-between">
+             <el-col :span="6"><div class="recom">推荐攻略</div></el-col>                      
+            <el-col :span="6"><el-button type="primary"  icon="el-icon-edit" >写游记</el-button></el-col>
+            </el-row>
+               
+               
+        </div>
+      
+       
+    </div>
+</template>
+
+<style>
+.searchbox{
+    position: absolute;
+    height: 140px;
+}
+.search{
+    width: 718px;
+    height: 36px;
+    padding-left: 15px;
+    border:2px solid #FEA410;
+   
+}
+ .icon{
+  color:#FEA410;
+  font-size: 22px;
+  font-weight: 700;
+  position: absolute;
+  left:700px;
+  top:10px;
+}
+.searchbox > p{
+    font-size:14px;
+    margin-top:10px;
+}
+.recommend{
+    width: 720px;
+    height: 40ppx;
+    border-bottom:1px solid #EDEDED;
+}
+.recom{
+    width: 75px;
+    height: 40px;
+    padding-top:20px;
+    color: #FEA410;
+    border-bottom:1px solid #FEA410;
+    
+    
+}
+
+   
+
+</style>
+
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        旅游首页
-    </div>
+    <el-container>
+  <el-aside width="430px">
+      <AsideNav />
+     
+  </el-aside>
+  <el-main >
+     <Search />
+   
+    
+  </el-main>
+</el-container>
 </template>
 
 <script>
+import AsideNav from '@/components/post/asideNav.vue';
+import Search from '@/components/post/search.vue';
+
 export default {
+
+components:{
+    AsideNav,
+    Search,
+   
+}
 
 }
 </script>
 
-<style>
+<style> 
+  
+  .el-aside {
 
+    color: #333;
+   
+  }
+  
+  .el-main {
+    /* background-color: #E9EEF3; */
+    color: #333;
+  
+  }
+  
+  body > .el-container {
+ 
+  }
+  
+ 
 </style>
