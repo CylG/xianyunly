ea003c33bffc97ca5e52bee29eed3b239974c8da
matrix01 <wtlee4511@163.com>
2019-07-07 15:18:59 +0800

    旅游攻略页面样式完成

--- components/post/search.vue
+++ components/post/search.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="searchbox">
-  
-        <input type="text" placeholder="请输入想去的地方，例如：广州" class="search"><i class="el-icon-search icon"></i>
-        <p>推荐：</p>
-
-        <div class="recommend">
-            <el-row type="flex" class="row-bg" justify="space-between">
-             <el-col :span="6"><div class="recom">推荐攻略</div></el-col>                      
-            <el-col :span="6"><el-button type="primary"  icon="el-icon-edit" @click="handleToCreate">写游记</el-button></el-col>
-            </el-row>
-               
-               
-        </div>
-      
-       
-    </div>
+<el-row class="searchbox">
+    <el-row type="flex" > 
+     <el-col :span="24">
+       <input type="text" placeholder="请输入想去的地方，例如：广州" class="search"><i class="el-icon-search icon"></i>
+     </el-col>
+    </el-row>
+    <el-row type="flex" > 
+     <el-col :span="24">
+          <div class="recom-hot">推荐：
+              <a href="">广州</a>
+              <a href="">上海</a>
+              <a href="">北京</a>
+          </div>
+     </el-col>
+    </el-row>      
+    <el-row type="flex" class="recommend" justify="space-between">
+        <el-col :span="12"><div class="recom">推荐攻略</div></el-col>                      
+        <el-col :span="3"><el-button type="primary"  icon="el-icon-edit" @click="handleToCreate">写游记</el-button></el-col>
+    </el-row> 
+ </el-row>        
 </template>
 
 <script>
@@ -26,22 +30,17 @@ export default {
                 path: "/post/create",
             })
         }
-    }
+    },     
 }
 </script>
 
 
 <style>
-.searchbox{
-    position: absolute;
-    height: 140px;
-}
 .search{
     width: 718px;
     height: 36px;
     padding-left: 15px;
-    border:2px solid #FEA410;
-   
+    border:2px solid #FEA410;   
 }
  .icon{
   color:#FEA410;
@@ -51,26 +50,23 @@ export default {
   left:700px;
   top:10px;
 }
-.searchbox > p{
-    font-size:14px;
-    margin-top:10px;
-}
+
 .recommend{
     width: 720px;
     height: 40ppx;
     border-bottom:1px solid #EDEDED;
+    margin-top:10px;
 }
 .recom{
     width: 75px;
     height: 40px;
     padding-top:20px;
     color: #FEA410;
-    border-bottom:1px solid #FEA410;
-    
-    
+    border-bottom:1px solid #FEA410;    
 }
-
-   
+.recom-hot{
+    font-size:12px;
+}   
 
 </style>
 
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,49 +1,85 @@
 <template>
-    <el-container>
-  <el-aside width="430px">
-      <AsideNav />
-     
-  </el-aside>
-  <el-main >
-     <Search />
-   
-    
-  </el-main>
-</el-container>
+  <el-container>
+    <el-aside width="430px">
+      <Nav />
+    </el-aside>
+    <el-main>
+      <Search />
+      <el-row class="recom-post" v-for='(item,index) in data' :key='index'>
+        <el-row type="flex" class="post-title">
+          <el-col :span="24">{{item.title}}  </el-col>
+        </el-row>
+         <el-row type="flex" class="post-content" >
+          <el-col :span="24"  v-html='item.summary'> </el-col>
+        </el-row>
+        <el-row class="post-img"  gutter="20">
+          <el-col :span="8" v-for="(item1,index) in item.images " :key='index'>
+            <img class='post-pic' :src='item1'/></el-col>    
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="4"><i class="el-icon-location-outline"></i>{{item.cityName}}</el-col>
+          <el-col :span="4">by<i class="el-icon-user"></i>{{item.account.nickname}}</el-col>
+          <el-col :span="4"><i class="el-icon-s-custom"></i>{{item.watch}}</el-col> 
+      </el-row>        
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
-import AsideNav from '@/components/post/asideNav.vue';
-import Search from '@/components/post/search.vue';
+import Nav from "@/components/post/nav.vue";
+import Search from "@/components/post/search.vue";
 
 export default {
+    components: {
+    Nav,
+    Search
+  },
 
-components:{
-    AsideNav,
-    Search,
-   
-}
-
-}
+   data(){
+        return {         
+            data: [],
+        }   
+    },
+    
+  mounted() {
+    this.$axios({
+      url: "/posts",
+      method: "GET"
+    }).then(res => {
+      const {data} = res.data;
+      this.data = data
+    
+    });
+  }
+};
 </script>
 
-<style> 
-  
-  .el-aside {
-
-    color: #333;
-   
-  }
-  
-  .el-main {
-    /* background-color: #E9EEF3; */
-    color: #333;
-  
-  }
-  
-  body > .el-container {
- 
-  }
-  
- 
+<style>
+.recom-post{  
+  width: 720px; 
+   border-bottom:1px solid #EDEDED;
+   font-size:14px;
+   color: #656565;
+      
+}
+.post-title{
+  font-size: 18px; 
+  margin:10px 0px;
+  color:black;
+}
+.post-content{
+  line-height: 20px;
+  height: 60px;
+  overflow: hidden; 
+}
+.post-img{
+ height: 154px;
+  overflow:hidden;
+}
+.post-pic{
+  width: 220px;
+  height: 150px;
+}
 </style>
+
